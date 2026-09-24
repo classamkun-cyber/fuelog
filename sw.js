eeed55bbf0100a01a92ba846e2443441a7a16019
classamkun-cyber <classa.m.kun@gmail.com>
2026-09-24 23:09:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fuelog-v1.0';
+const CACHE = 'fuelog-v1.10';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.png', './icon-192.png', './icon-180.png'];
 
 self.addEventListener('install', (e) => {
